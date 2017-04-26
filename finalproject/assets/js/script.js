3446fb6c67647fe7b68164e66cf2f61e6ba2a590
RoannaLin <roanna.lin@gmail.com>
2017-04-25 23:52:05 -0700

    Final Project

---
+++ finalproject/assets/js/script.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+	$("#egtitle").hover(function() {
+		$("#egtitle").css("opacity", 1);
+	});
+		$("#egtitle").mouseout(function() {
+		$("#egtitle").css("opacity", 0.7);
+	});
+
+	$("#lttitle").hover(function() {
+		$("#lttitle").css("opacity", 1);
+	});
+		$("#lttitle").mouseout(function() {
+		$("#lttitle").css("opacity", 0.7);
+	});
+
+		$("#botitle").hover(function() {
+		$("#botitle").css("opacity", 1);
+	});
+		$("#botitle").mouseout(function() {
+		$("#botitle").css("opacity", 0.7);
+	});
+
+});
